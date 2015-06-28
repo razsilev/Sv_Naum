6c6589a127a83bb47cb839a6b7d38d73328de586
razsilev <grasilev@gmail.com>
2015-06-28 21:30:20 +0300

    Remove filter for handle error attribute.

--- Source/SvNaum.Web/App_Start/FilterConfig.cs
+++ Source/SvNaum.Web/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace SvNaum.Web
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            //filters.Add(new HandleErrorAttribute());
         }
     }
 }

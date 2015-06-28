9f404eb1766be1c10951d8db7fc786e4172bf1a7
razsilev <grasilev@gmail.com>
2015-06-28 12:10:45 +0300

    Make paging over sermons.

--- Source/SvNaum.Web/Controllers/BaseController.cs
+++ Source/SvNaum.Web/Controllers/BaseController.cs
@@ -29,7 +29,7 @@
         private string GetMongoDbConnectionString()
         {
             return ConfigurationManager.AppSettings.Get("MONGOLAB_URI") ??
-                //"mongodb://localhost:27017" ??
+                //"mongodb://localhost:27017";
                 "mongodb://appharbor_1aca7d3f-ed85-4975-8b3f-56a35407755d:t9q99k30fvct2u51kt09jv1pqa@ds039231.mongolab.com:39231/appharbor_1aca7d3f-ed85-4975-8b3f-56a35407755d";
         }
     }

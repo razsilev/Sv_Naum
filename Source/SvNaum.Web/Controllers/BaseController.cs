62767105875151e18b015c46219c3da8c413c9f7
razsilev <grasilev@gmail.com>
2015-07-01 15:13:16 +0300

    Add left aside with last ten Sermons and Payers.

--- Source/SvNaum.Web/Controllers/BaseController.cs
+++ Source/SvNaum.Web/Controllers/BaseController.cs
@@ -2,9 +2,13 @@
 {
     using System.Configuration;
     using System.Web.Mvc;
+    using System.Linq;
+
     using MongoDB.Driver;
 
     using SvNaum.Data;
+    using System.Collections.Generic;
+    using SvNaum.Models;
 
     public abstract class BaseController : Controller
     {
@@ -16,6 +20,9 @@
 
             var conString = this.GetMongoDbConnectionString();
             this.context = new SvNaumDbContext(conString);
+
+            this.PopulateTopTenSermons();
+            this.PopulateTopTenBreviaries();
         }
 
         public SvNaumDbContext Context
@@ -31,6 +38,49 @@
             return ConfigurationManager.AppSettings.Get("MONGOLAB_URI") ??
                 //"mongodb://localhost:27017";
                 "mongodb://appharbor_1aca7d3f-ed85-4975-8b3f-56a35407755d:t9q99k30fvct2u51kt09jv1pqa@ds039231.mongolab.com:39231/appharbor_1aca7d3f-ed85-4975-8b3f-56a35407755d";
+        }
+
+        private void PopulateTopTenSermons()
+        {
+            var sermons = new List<Sermon>();
+
+            try
+            {
+                sermons = this.Context.Sermons.FindAll().OrderByDescending(s => s.Date).Take(10).ToList();
+
+                if (sermons == null)
+                {
+                    sermons = new List<Sermon>();
+                }
+            }
+            catch (System.Exception)
+            {
+
+            }
+
+            this.ViewBag.TopTenSermons = sermons;
+        }
+
+        private void PopulateTopTenBreviaries()
+        {
+
+            var prayers = new List<Prayer>();
+
+            try
+            {
+                prayers = this.Context.Prayers.FindAll().Reverse().Take(10).ToList();
+
+                if (prayers == null)
+                {
+                    prayers = new List<Prayer>();
+                }
+            }
+            catch (System.Exception)
+            {
+
+            }
+
+            this.ViewBag.TopTenPrayers = prayers;
         }
     }
 }

88937f419a7c6c4e7438beb27bdc9569638dbaf5
razsilev <grasilev@gmail.com>
2015-07-03 19:08:02 +0300

    Add project again.

---
+++ Source/SvNaum.Models/Ministration.cs
@@ -0,0 +1,21 @@
+﻿namespace SvNaum.Models
+{
+    using System;
+
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+
+    public class Ministration
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Description { get; set; }
+
+        public string DayName { get; set; }
+    }
+}
---
+++ Source/SvNaum.Models/Paterik.cs
@@ -0,0 +1,20 @@
+﻿namespace SvNaum.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+
+    public class Paterik
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        public string Author { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+    }
+}
